7e7989f02234baef025522508e77f61a04745c02
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-03-30 12:28:39 +0100

    gnome config add forge and multiple settings

--- home-manager/features/desktop/gnome.nix
+++ home-manager/features/desktop/gnome.nix
@@ -1,40 +1,57 @@
 { pkgs, ... }: {
-  # /org/gnome/shell/extensions/dash-to-panel/panel-element-positions
+  # Do `dconf watch /` to see which keys to set when changing settings
   dconf.settings = {
-    "org/gnome/shell" = {
+    "org/gnome" = {
+      "desktop" = {
+        "wm" = {
+          "preferences/button-layout" = "appmenu:minimize,maximize,close";
 
-      favorite-apps = [
-        "firefox.desktop"
-        "code.desktop"
-        "org.gnome.Terminal.desktop"
-        "org.gnome.Nautilus.desktop"
-      ];
+          keybindings = {
+            switch-to-workspace-right = [ "<Alt><Super>Right" ];
+            switch-to-workspace-left = [ "<Alt><Super>Left" ];
+            move-to-workspace-left = [ "<Control><Super>Left" ];
+            move-to-workspace-right = [ "<Control><Super>Right" ];
+          };
+        };
+        "interface" = {
+          show-battery-percentage = true;
+        };
+      };
+      "shell" = {
+
+        favorite-apps = [
+          "firefox.desktop"
+          "code.desktop"
+          "org.gnome.Terminal.desktop"
+          "org.gnome.Nautilus.desktop"
+        ];
 
 
 
-      disable-user-extensions = false;
+        disable-user-extensions = false;
 
-      # `gnome-extensions list` for a list
-      enabled-extensions = [
-        "trayIconsReloaded@selfmade.pl"
-        "Vitals@CoreCoding.com"
-        "dash-to-panel@jderose9.github.com"
-        "sound-output-device-chooser@kgshank.net"
-        "space-bar@luchrioh"
-      ];
+        # `gnome-extensions list` for a list
+        enabled-extensions = [
+          "trayIconsReloaded@selfmade.pl"
+          "Vitals@CoreCoding.com"
+          "dash-to-panel@jderose9.github.com"
+          "space-bar@luchrioh"
+          "forge@jmmaranan.com"
 
-      "extensions/dash-to-panel/panel-element-positions" = ''
-        {"0":[{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"showAppsButton","visible":true,"position":"centerMonitor"},{"element":"taskbar","visible":true,"position":"centerMonitor"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]}
-      '';
+        ];
+
+        "extensions/dash-to-panel/panel-element-positions" = ''
+          {"0":[{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"showAppsButton","visible":true,"position":"centerMonitor"},{"element":"taskbar","visible":true,"position":"centerMonitor"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]}
+        '';
+      };
+
     };
-
+    home.packages = with pkgs; [
+      gnomeExtensions.tray-icons-reloaded
+      gnomeExtensions.vitals
+      gnomeExtensions.dash-to-panel
+      gnomeExtensions.space-bar
+      gnomeExtensions.forge
+    ];
   };
-  home.packages = with pkgs; [
-    # ...
-    gnomeExtensions.tray-icons-reloaded
-    gnomeExtensions.vitals
-    gnomeExtensions.dash-to-panel
-    gnomeExtensions.sound-output-device-chooser
-    gnomeExtensions.space-bar
-  ];
 }
