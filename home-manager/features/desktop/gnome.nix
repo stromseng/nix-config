aa42574310eb59649eae064577afe9cf87d91854
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-04-01 11:43:03 +0200

    disable dconf error command, idk why it doesnt work

--- home-manager/features/desktop/gnome.nix
+++ home-manager/features/desktop/gnome.nix
@@ -7,9 +7,9 @@
   # Value: [firefox.desktop, ...]
 
   dconf.settings = {
-    "/org/gnome/desktop/wm/preferences" = {
-      button-layout = "appmenu:minimize,maximize,close";
-    };
+    # "/org/gnome/desktop/wm/preferences" = {
+    #   button-layout = "appmenu:minimize,maximize,close";
+    # };
     "org/gnome/desktop/wm/keybindings" = {
       switch-to-workspace-right = [ "<Alt><Super>Right" ];
       switch-to-workspace-left = [ "<Alt><Super>Left" ];
