b6b983c21fa83e66e81f7c3f0f00ced1f99483a3
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-04-19 13:32:14 +0200

    docs

--- nixos/common/optional/kde.nix
+++ nixos/common/optional/kde.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
+
+  # Enable the SDDM display manager and the Plasma 6 desktop.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
