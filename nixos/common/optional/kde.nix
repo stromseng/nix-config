a5c79e90f805bd7a547d0f6f283069fbec612e1d
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-04-19 13:29:23 +0200

    update renamed settings

--- nixos/common/optional/kde.nix
+++ nixos/common/optional/kde.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   qt = {
     enable = true;
