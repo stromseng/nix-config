46222924d979c75ff1a8a9c0f42d18fe0c158d09
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-05-03 17:06:24 +0200

    add substring search

--- home-manager/features/cli/zsh.nix
+++ home-manager/features/cli/zsh.nix
@@ -23,7 +23,8 @@
         znap source marlonrichert/zsh-autocomplete
         znap source zsh-users/zsh-autosuggestions
         znap source zsh-users/zsh-syntax-highlighting
-        znap source zsh-users/zsh-completions        
+        znap source zsh-users/zsh-completions
+        znap source zsh-users/zsh-history-substring-search
       '';
     };
   };
