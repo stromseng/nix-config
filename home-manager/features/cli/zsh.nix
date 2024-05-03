54cf0388d79497aa69e9775e94ec228512aaead8
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-05-03 16:59:41 +0200

    move to znap for zsh plugins

--- home-manager/features/cli/zsh.nix
+++ home-manager/features/cli/zsh.nix
@@ -12,13 +12,17 @@
       initExtraBeforeCompInit = ''
       '';
       initExtra = ''
+        # Download Znap, if it's not there yet.
+        [[ -r ~/repos/znap/znap.zsh ]] ||
+            git clone --depth 1 -- \
+                https://github.com/marlonrichert/zsh-snap.git ~/repos/znap
+        source ~/repos/znap/znap.zsh  # Start Znap
+
+
+        # `znap source` starts plugins.
+        znap source marlonrichert/zsh-autocomplete
+        znap source zsh-users/zsh-autosuggestions
       '';
-      zplug = {
-        enable = true;
-        plugins = [
-          { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-        ];
-      };
     };
   };
 }
