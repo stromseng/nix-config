767971d89964d64bb2123692fbef1d96934edb7d
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-05-14 13:15:03 +0200

    rempve surrealdb cuz of long build times

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -85,8 +85,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    surrealist2 # SurrealDB interface, custom package imported in flake.nix and defined in /pkgs
-    inputs.surrealDB.packages.${pkgs.stdenv.system}.default
+    #surrealist2 # SurrealDB interface, custom package imported in flake.nix and defined in /pkgs
+    #inputs.surrealDB.packages.${pkgs.stdenv.system}.default
   ];
 
 
